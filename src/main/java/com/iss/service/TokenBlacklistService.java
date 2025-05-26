bf2d92c39d239fb8b07529c9a3c413290bd4d164
Vinayak-RBK <vinayakbhilawadi95@gmail.com>
2025-05-26 10:52:06 +0530

    deleted src folder

--- src/main/java/com/iss/service/TokenBlacklistService.java
+++
@@ -1,9 +0,0 @@
-package com.iss.service;
-
-public interface TokenBlacklistService {
-
-    public void blacklistToken(String token); 
-
-    public boolean isTokenBlacklisted(String token);
-	
-}
--- src/main/java/com/iss/test/TestEntity.java
+++
@@ -1,7 +0,0 @@
-package com.iss.test;
-
-public class TestEntity {
-	private String name ;
-	private String age;
-
-}
